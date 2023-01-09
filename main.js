fb4d232f09af52505cf41df7f84383250f43d399
Alex Torres <47037870+alextedtech@users.noreply.github.com>
2023-01-08 21:48:02 -0800

    Compelted Login Form - missing mobile functions

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
 /*=============== SHOW HIDDEN - PASSWORD ===============*/
+const showHiddenPass = (inputPass,inputIcon)=>{
+    const input = document.getElementById(inputPass),
+        iconEye = document.getElementById(inputIcon)
+
+        iconEye.addEventListener('click', () =>{
+            //Change Password to text
+            if(input.type === 'password'){
+                //switch to text
+                input.type = 'text'
+                //add icon
+                iconEye.classList.add('ri-eye-line')
+                //remove icon
+                iconEye.classList.remove('ri-eye-off-line')
+            }else{
+                //Change to password
+                input.type = 'password'
+
+                //Remove Icon
+
+                iconEye.classList.remove('ri-eye-line')
+
+                //Add Icon
+                iconEye.classList.add('ri-eye-off-line')
+            }
+        })
+}
+
+showHiddenPass('input-pass','input-icon')
